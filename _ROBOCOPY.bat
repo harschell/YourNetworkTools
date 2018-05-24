b11c9eca47edb2b624657aad975cc0ad6f89997a
Esteban <services@yourvrexperience.com>
2018-05-24 13:14:14 +0200

    ROBOCOPY batch file is custom for each project and should not be included in the repository.

--- _ROBOCOPY.bat
+++
@@ -1,2 +0,0 @@
-robocopy C:\Users\Esteban\Desktop\GitHub\ExamplesNetworkTools\Assets\Submodules C:\Users\Esteban\Desktop\GitHub\ExamplesNetworkTools\__CLIENTS\Examples_Client1\Assets\Submodules /MIR
-pause
